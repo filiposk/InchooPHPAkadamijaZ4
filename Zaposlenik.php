24f930c27ab8012cb4717babf3e456d584d14e7e
filiposk <fpavlovic1995@gmail.com>
2019-01-28 14:31:38 +0100

    magic methods

--- Zaposlenik.php
+++ Zaposlenik.php
@@ -9,145 +9,125 @@ class Zaposlenik
     private $spol;
     private $primanja;
 
-    public static $sviZaposlenici = array;
+
     public function __construct($id, $ime, $prezime, $datumRodenja, $spol, $primanja)
     {
-        $this->getId($id);
-        $this->getIme($ime);
-        $this->getPrezime($prezime);
-        $this->getDatumRodenja($datumRodenja);
-        $this->getSpol($spol);
-        $this->getPrimanja($primanja);
+        $this->id($id);
+        $this->ime($ime);
+        $this->prezime($prezime);
+        $this->datumRodenja($datumRodenja);
+        $this->spol($spol);
+        $this->primanja($primanja);
 
-        return $sviZaposlenici[] = $this;
     }
 
-    /**
-     * @return mixed
-     */
-    public function getId()
+    public function __get($key)
     {
-        return $this->id;
+        return array_key_exists($key, $this->data) ? $this->data[$key] : "undefined key";
     }
-
-    /**
-     * @param mixed $id
-     */
-    public function setId($id)
+    public function __set($key, $value)
     {
-        $this->id = $id;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getIme()
-    {
-        return $this->ime;
-    }
-
-    /**
-     * @param mixed $ime
-     */
-    public function setIme($ime)
-    {
-        $this->ime = $ime;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getPrezime()
-    {
-        return $this->prezime;
-    }
-
-    /**
-     * @param mixed $prezime
-     */
-    public function setPrezime($prezime)
-    {
-        $this->prezime = $prezime;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getDatumRodenja()
-    {
-        return $this->datumRodenja;
-    }
-
-    /**
-     * @param mixed $datumRodenja
-     */
-    public function setDatumRodenja($datumRodenja)
-    {
-        $this->datumRodenja = $datumRodenja;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getSpol()
-    {
-        return $this->spol;
-    }
-
-    /**
-     * @param mixed $spol
-     */
-    public function setSpol($spol)
-    {
-        $this->spol = $spol;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getPrimanja()
-    {
-        return $this->primanja;
-    }
-
-    /**
-     * @param mixed $primanja
-     */
-    public function setPrimanja($primanja)
-    {
-        $this->primanja = $primanja;
+        $this->data[$key] = $value;
     }
 
 
 
-//    public function __get($property) {
-//        if (property_exists($this, $property)) {
-//            return $this->$property;
-//        }
-//    }
-//
-//    public function __set($property, $value) {
-//        if (isset($property)) {
-//            $this->$property = $value;
-//        }else{
-//            echo 'potrebno je unjeti sve podatke';
-//        }
-//
-//        return $this;
-//    }
-
-
-//    public function Ispis()
+//    /**
+//     * @return mixed
+//     */
+//    public function getId()
 //    {
-//        return $this->$zaposlenik->ime;
+//        return $this->id;
+//    }
+//
+//    /**
+//     * @param mixed $id
+//     */
+//    public function setId($id)
+//    {
+//        $this->id = $id;
+//    }
+//
+//    /**
+//     * @return mixed
+//     */
+//    public function getIme()
+//    {
+//        return $this->ime;
+//    }
+//
+//    /**
+//     * @param mixed $ime
+//     */
+//    public function setIme($ime)
+//    {
+//        $this->ime = $ime;
+//    }
+//
+//    /**
+//     * @return mixed
+//     */
+//    public function getPrezime()
+//    {
+//        return $this->prezime;
+//    }
+//
+//    /**
+//     * @param mixed $prezime
+//     */
+//    public function setPrezime($prezime)
+//    {
+//        $this->prezime = $prezime;
+//    }
+//
+//    /**
+//     * @return mixed
+//     */
+//    public function getDatumRodenja()
+//    {
+//        return $this->datumRodenja;
+//    }
+//
+//    /**
+//     * @param mixed $datumRodenja
+//     */
+//    public function setDatumRodenja($datumRodenja)
+//    {
+//        $this->datumRodenja = $datumRodenja;
+//    }
+//
+//    /**
+//     * @return mixed
+//     */
+//    public function getSpol()
+//    {
+//        return $this->spol;
+//    }
+//
+//    /**
+//     * @param mixed $spol
+//     */
+//    public function setSpol($spol)
+//    {
+//        $this->spol = $spol;
+//    }
+//
+//    /**
+//     * @return mixed
+//     */
+//    public function getPrimanja()
+//    {
+//        return $this->primanja;
+//    }
+//
+//    /**
+//     * @param mixed $primanja
+//     */
+//    public function setPrimanja($primanja)
+//    {
+//        $this->primanja = $primanja;
 //    }
 }
 
 
-$zaposlenik = new Zaposlenik();
-$zaposlenik->setId(1);
-$zaposlenik->setIme('Filip');
-$zaposlenik->setPrezime('Pavlović');
-$zaposlenik->setDatumRodenja('10-10-1989');
-$zaposlenik->setSpol(1);
-$zaposlenik->setPrimanja(300);
+
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 require "Zaposlenik.php";
 
+$sviZaposlenici = [];
 while( true ) {
 
     // Print the menu on console
@@ -20,18 +21,21 @@ while( true ) {
 
         case 1:
         {
-            print_r(Zaposlenik::$sviZaposlenici);
-            echo "Želite li se vratiti na izbornik? (DA/NE)\n";
-            if (strtolower(trim(fgets(STDIN))) !== 'da') {
-                $bool = false;
+            {
+                ispisiZaposlenike($sviZaposlenici);
+                echo "Želite li se vratiti na izbornik? (DA/NE)\n";
+                if (strtolower(trim(fgets(STDIN))) !== 'da') {
+                    $bool = false;
+                }
+                break;
             }
-
-
-            break;
         }
         case 2:
-        {
+        {{
+            echo "Upišite sve potrebne podatke: \n";
+            $sviZaposlenici[] = unosZaposlenika($sviZaposlenici);
             break;
+        }
         }
         case 3:
         {
@@ -59,7 +63,93 @@ function ispisIzbornika() {
     echo "6 - Izlaz\n";
 }
 
-function chooseSource() {
+function ispisiZaposlenike($array) {
+    echo "*************************************************\n";
+    for ($i = 0; $i < count($array); $i++) {
+        echo "ID: " . $array[$i]->getId() . "\n";
+        echo "IME: " . $array[$i]->getIme(). "\n";
+        echo "PREZIME: " . $array[$i]->getPrezime()."\n";
+        echo "DATUM ROĐENJA: " . $array[$i]->getDatumRodenja()."\n";
+        echo "SPOL: " . $array[$i]->getSpol()."\n";
+        echo "MJESEČNA PRIMANJA: " . $array[$i]->getPrimanja()."\n";
+        echo "*************************************************\n";
+    }
+}
 
-    // Logic to choose source location
+function unosZaposlenika($array = null)
+{
+    echo "ID: ";
+    $id = kontrolaId(readline(),$array);
+    echo "Ime: ";
+    $ime = kontrolaImenaIPrezimena(readline());
+    echo "Prezime: ";
+    $prezime = kontrolaImenaIPrezimena(readline());
+    echo "Datum rođenja (dd.mm.yyyy): ";
+    $datumRodenja = kontrolaDatum(readline());
+    echo "Spol: ";
+    $spol = kontrolaSpol(readline());
+    echo "Mjesečna primanja: ";
+    $mjesecnaPrimanja = kontrolaMjesecnoPrimanje(readline());
+    return new Zaposlenik($id, $ime, $prezime, $datumRodenja, $spol, $mjesecnaPrimanja);
+}
+
+function kontrolaImenaIPrezimena($var)
+{
+    if($var==="" || preg_match('~[0-9]+~', $var)){
+        echo "Ime/prezime ne može biti prazno i sadržavati brojeve.\nUnesite novo ime/prezime:\n";
+        return kontrolaImenaIPrezimena(readline());
+    }else{
+        return $var;
+    }
+}
+function kontrolaId($var,$array)
+{
+    if($var===""){
+        echo "Morate unijeti ID.\nUnesite novi ID:\n";
+        return kontrolaId(readline(),$array);
+    }
+    for($i=0;$i<count($array);$i++){
+        if($array[$i]->getId()===$var ){
+            echo "Zaposlenik s istim ID-em već postoji.\nUnesite novi ID:\n";
+            return kontrolaId(readline(),$array);
+        }
+    }
+    return $var;
+}
+
+function kontrolaMjesecnoPrimanje($var)
+{
+    var_dump($var);
+    $var = floatval(str_replace(",",".",$var));
+    var_dump($var);
+    if($var==="" || !is_float($var) || $var<=0){
+        echo "Mjesečno primanje mora biti decimalan broj veći od 0.\nUnesite novu vrijednost:\n";
+        return kontrolaMjesecnoPrimanje(readline());
+    }else{
+        return number_format($var,2,'.','');
+    }
+}
+
+function kontrolaDatum($var)
+{
+    $var = str_replace(["-","/","'\'"], ".",$var);
+    $format = "d.m.Y";
+    $d = DateTime::createFromFormat($format, $var);
+    if($var==="" || preg_match("/^[a-zA-Z]+$/", $var) || $d->format($format)!==$var){
+        echo "Morate upisati datum formata dd.mm.yyyy.\nUnesite novu vrijednost:\n";
+        return kontrolaDatum(readline());
+    }else{
+        return $var;
+    }
+}
+
+function kontrolaSpol($var)
+{
+    var_dump($var);
+    if($var==="" || ($var !== "muški" && $var !== "ženski")){
+        echo "Spol može biti muški ili ženski.\nUnesite novu vrijednost:\n";
+        return kontrolaSpol(readline());
+    }else{
+        return $var;
+    }
 }
